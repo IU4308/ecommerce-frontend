d2c967407d7dcf545203ea843c21537a7bbe900a
Ilya <ilya671qw@gmail.com>
2025-07-09 17:54:47 +0300

    update

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useCartStore } from '../store/useCartStore';
 
 export default function AppLayout() {
-    // const [cartIsOpen, setCartIsOpen] = useState(false);
     const cartIsOpen = useCartStore((state) => state.cartIsOpen);
     const closeCart = useCartStore((state) => state.closeCart);
 
@@ -23,10 +22,7 @@ export default function AppLayout() {
 
     return (
         <div className="relative max-w-[1500px] mx-auto">
-            <Header
-            // cartIsOpen={cartIsOpen}
-            // onToggleCart={() => setCartIsOpen((open) => !open)}
-            />
+            <Header />
 
             {/* Gray overlay when cart is open */}
             {cartIsOpen && (
