27e1e0a9487aa564639bfd56ea67fa18a4233ed8
Ilya <ilya671qw@gmail.com>
2025-07-21 16:07:45 +0300

    debug

--- src/components/pdp/ProductGallery.tsx
+++ src/components/pdp/ProductGallery.tsx
@@ -1,16 +1,14 @@
 export default function ProductGallery({ imageUrl }: { imageUrl: string }) {
     return (
         <div
-            className="flex-1 h-full overflow-auto scrollbar-thin"
+            className="flex-1 h-full flex items-center justify-center" // Center image
             data-testid="product-gallery"
         >
-            <div className="w-full h-full xl:w-max xl:h-max xl:min-w-full xl:min-h-full ">
-                <img
-                    src={imageUrl}
-                    className="object-contain max-w-full max-h-full xl:max-w-none xl:max-h-none "
-                    alt="Product"
-                />
-            </div>
+            <img
+                src={imageUrl}
+                alt="Product"
+                className="max-w-full max-h-full object-contain"
+            />
         </div>
     );
 }
